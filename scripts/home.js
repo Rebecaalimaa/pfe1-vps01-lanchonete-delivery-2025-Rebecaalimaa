f1fa034083ee6ba0fc37b80306dfdc28403d6903
Rebecaalimaa <rebecalimanascimento3@gmail.com>
2025-04-23 08:24:27 -0300

    readme

--- scripts/home.js
+++ scripts/home.js
@@ -1,4 +1,12 @@
 
+const produtos = [
+    { id: 1, nome: "Hamburguer", preco: 15.00 },
+    { id: 2, nome: "X-Burguer", preco: 18.00 },
+    { id: 3, nome: "X-Salada", preco: 20.00 },
+    { id: 4, nome: "X-Bacon", preco: 25.00 },
+    { id: 5, nome: "X-Egg", preco: 30.00 },
+    { id: 6, nome: "X-Tudo", preco: 35.00 },
+];
 
 const clienteInput = document.getElementById("cliente");
 const enderecoInput = document.getElementById("endereco");
